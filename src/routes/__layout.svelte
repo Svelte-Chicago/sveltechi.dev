b395020620bc254389b817b416f21aebda82002e
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2022-04-29 15:43:33 -0500

    feat(index): additional assets and layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>Svelte Society - Chicago</title>
+    <link rel="icon" href="/svelte_chicago_125x125.png" />
+</svelte:head>
+
+<slot />
