f93b162920ccbfe15d5f5cfeaae4ff38ad3047c5
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2022-04-29 19:03:30 -0500

    fix(index): layout consistency, revision

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,5 +6,7 @@
     <title>Svelte Society - Chicago</title>
     <link rel="icon" href="/svelte_chicago_125x125.png" />
 </svelte:head>
+<div class="flex flex-col items-center max-h-screen">
+    <slot />
+</div>
 
-<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,26 +4,26 @@
 </script>
 
 
-<div class='w-full relative'>
+<div class='w-full relative max-w-4xl h-screen text-center border-red-600 border-l-2 border-r-2'>
     <img src="/chicago_flag.svg" class='w-full' alt="Flag of the City of Chicago"/>
-    <div class="absolute bottom-6 left-6 align-middle ">
+    <div class="absolute bottom-[15rem] left-6 align-middle ">
         <div class="border-red-600 lg:h-[10rem] lg:w-[10rem] sm:h-[7rem] sm:w-[7rem] bg-white border-8 rounded-full p-4 align-middle inline-block">
             <img src="/svelte_chicago_125x125.png" alt="Svelte Society Chicago Logo" />
         </div>
-        <div class="text-red-600 g:h-[10rem] lg:w-[10rem] sm:h-[7rem] sm:w-[7rem] inline-block pl-10 pt-10 align-middle">
-            <a href="https://twitter.com/SvelteChicago"><Icon data={twitter} scale="4" /></a>
+        <div class="text-red-600 g:h-[10rem] lg:w-[10rem] sm:h-[7rem] sm:w-[7rem] pt-6 inline-block rounded-full align-middle pl-6">
+            <a href="https://twitter.com/SvelteChicago"><Icon data={twitter} scale="4" class="bg-white w-full h-full border-2 border-red-600 rounded-full" /></a>
         </div>
-        <div class="text-red-600 g:h-[10rem] lg:w-[10rem] sm:h-[7rem] sm:w-[7rem] inline-block pt-10 align-middle">
-            <a href="https://github.com/svelte-chicago/"><Icon data={github} scale="4" /></a>
+        <div class="text-red-600 g:h-[10rem] lg:w-[10rem] sm:h-[7rem] sm:w-[7rem] pt-6 inline-block rounded-full align-middle pl-6 ">
+            <a href="https://github.com/svelte-chicago/"><Icon data={github} scale="4" class="bg-white w-full h-full border-2 border-red-600 rounded-full" /></a>
+        </div>
+    </div>
+    <div class='absolute bottom-[8rem] left-[8rem] text-left'>
+        <div class="text-4xl">
+            Svelte Society: Chicago
+        </div>
+        <div class="text-xl">
+            We're just getting started - find us on twitter and github
         </div>
     </div>
 </div>
-<div class='w-full relative'>
-    <div class="text-4xl absolute left-[12rem]">
-        Svelte Society: Chicago
-    </div>
-    <div class="text-xl absolute top-12 left-[12rem]">
-        We're just getting started - find us on twitter and github
-    </div>
 
-</div>
