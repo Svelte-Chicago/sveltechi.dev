4b639402ad56b0d7e99cd616ce49b1bd65cbb7eb
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2022-05-03 10:08:40 -0500

    fix(events): added rewrite rules, refactored firestore integration

--- api/[handler].py
+++ api/[handler].py
@@ -6,10 +6,9 @@ from datetime import datetime
 import json
 
 try:
-    # handle local envvar while vercel is broken
-    creds_env = os.getenv("GA") if os.getenv("GA") else os.getenv("GOOGLE_AUTH")
-    cred = credentials.Certificate(json.loads(creds_env))
-    f = firebase_admin.initialize_app(cred, {"projectId": "sveltechi-dev"})
+    creds = json.loads(os.getenv("GOOGLE_AUTH"))
+    certificate = credentials.Certificate(creds)
+    f = firebase_admin.initialize_app(certificate, {"projectId": creds['project_id']})
 except Exception as e:
     print(f"{e}")
     # exit()
@@ -28,7 +27,7 @@ class MailHandler:
         response.media = {"mail": "email"}
 
 
-class EventHandler:
+class EventsHandler:
     def on_get(self, request, response) -> None:
         db = firestore.client()
 
@@ -42,13 +41,25 @@ class EventHandler:
         documents = []
         for doc in docs_generator:
             doc_dict = doc.to_dict()
+            doc_dict["id"] = doc.id
             doc_dict["Date"] = doc_dict["Date"].rfc3339()
             documents.append(doc_dict)
 
         response.media = {"events": documents}
 
 
+class SingleEventHandler:
+    def on_get(self, request, response) -> None:
+        db = firestore.client()
+
+        events_ref = (
+            db.collection("Events").where("Date", ">=", datetime.now())
+            if not request.get_param("old")
+            else db.collection("Events").where("Date", "<=", datetime.now())
+        )
+
 app = falcon.App(cors_enable=True)
 app.add_route(f"{base_route}/handler", BaseHandler())
 app.add_route(f"{base_route}/mail", MailHandler())
-app.add_route(f"{base_route}/events", EventHandler())
+app.add_route(f"{base_route}/events", EventsHandler())
+app.add_route(f"{base_route}/events/{{id}}", EventsHandler())
